47a7659901b353228e078df770fe7e9c8c657e49
Ser-Avi <128562182+Ser-Avi@users.noreply.github.com>
2025-03-21 14:38:46 -0400

    Imported LSystem Code

--- TreeVigorate/pluginMain.cpp
+++ TreeVigorate/pluginMain.cpp
@@ -1,11 +1,36 @@
 #include <maya/MFnPlugin.h>
 #include <maya/MGlobal.h>
+#include "TreeNode.h"
+
+#include <maya/MPxCommand.h>
+#include <maya/MIOStream.h>
+#include <maya/MString.h>
+#include <maya/MArgList.h>
+#include <maya/MSimple.h>
+#include <maya/MDoubleArray.h>
+#include <maya/MPoint.h>
+#include <maya/MPointArray.h>
+#include <maya/MFnNurbsCurve.h>
+#include <maya/MDGModifier.h>
+#include <maya/MPlugArray.h>
+#include <maya/MVector.h>
+#include <maya/MFnDependencyNode.h>
+#include <maya/MStringArray.h>
 
 MStatus initializePlugin(MObject obj) {
 	const char* pluginVendor = "Avi and Eli :)";
 	const char* pluginVersion = "0.2";
 
-	MFnPlugin fnPlugin(obj, pluginVendor, pluginVersion);
+	MStatus   status = MStatus::kSuccess;
+	MFnPlugin plugin(obj, pluginVendor, pluginVersion);
+
+	// adding TreeNode
+	status = plugin.registerNode("TreeNode", TreeNode::id,
+		TreeNode::creator, TreeNode::initialize);
+	if (!status) {
+		status.perror("registerNode");
+		return status;
+	}
 
 	MGlobal::displayInfo("Plugin initialized successfully!");
 
@@ -13,6 +38,15 @@ MStatus initializePlugin(MObject obj) {
 }
 
 MStatus uninitializePlugin(MObject obj) {
+	MStatus   status = MStatus::kSuccess;
+	MFnPlugin plugin(obj);
+
+	status = plugin.deregisterNode(TreeNode::id);
+	if (!status) {
+		status.perror("deregisterNode");
+		return status;
+	}
+
 	MGlobal::displayInfo("Plugin unitialized successfully!");
 
 	return(MS::kSuccess);
